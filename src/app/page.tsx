5d686b6df5bb2a0866510628d801c2618d33686a
Achille DAVID <davidachille18@gmail.com>
2024-05-02 12:31:32 +0200

    fix(landing-page): traduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,16 @@ import { Button } from "@/components/ui/button";
 import { Fragment } from "react";
 import style from "./home.module.scss"
 import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
     <Fragment>
       <Header />
       <main className={style.container}>
-        <h1>Everything you are.<br />In one, simple link in bio.</h1>
-        <p>Create a page with everything about you in one link. Share it with your friends and followers.</p>
-        <Button asChild><Link href="/sign-in" >Get started</Link></Button>
+        <h1>Tout sur vous en un seul clic.<br />À travers un seul lien.</h1>
+        <p>Rassemblez vos liens personnalisés en un seul endroit pour une expérience simplifiée.<br />Partagez-la avec vos amis et abonnés.</p>
+        <Button asChild><Link href="/sign-in" >Commencer<ArrowRight className="w-4 h-4 ml-2" /></Link></Button>
       </main>
     </Fragment >
   );
